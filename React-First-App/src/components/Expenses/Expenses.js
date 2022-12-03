70b0d2b73e7101788a948a6452e7a2bbe6e38bca
othman31 <othman0031@gmail.com>
2022-12-03 20:59:03 +0100

    organizing components

--- React-First-App/src/components/Expenses/Expenses.js
+++ React-First-App/src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 function Expenses(props) {
   // After props always should be expenses because we declared it in App.js as expenses=expenses_
   return (
